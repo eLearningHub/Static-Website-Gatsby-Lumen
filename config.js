8cd0bc3e020b13c82651aee9a16d884ef25d47cb
Behzad Samadi <behzad.samadi@gmail.com>
2020-07-14 21:26:15 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  url: 'https://elearninghub.github.io',
+  url: 'https://elearninghub.github.io/Static-Website-Gatsby-Lumen',
   pathPrefix: '/Static-Website-Gatsby-Lumen',
   title: 'Blog by John Doe',
   subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',

28b35fde3c97f9f1d2264f9a55a5e7494ae0c440
bsamadi <bsamadi@users.noreply.github.com>
2020-07-15 01:29:00 +0000

    deploy: 8cd0bc3e020b13c82651aee9a16d884ef25d47cb

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-7b67524382cfd0355686.js"
+    "url": "webpack-runtime-4558555bf44aca1ba017.js"
   },
   {
     "url": "styles.69c8303230227041b58c.css"
@@ -46,7 +46,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b40d3209a4c09f3e1058ff707c693fc3"
+    "revision": "55283af34c2cc80afcdb422e1bb2204f"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-c9875e2473a7c518755f.js"
@@ -57,7 +57,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "8096f613a96100af43eb8b9ca5249cef"
+    "revision": "3b8ed417e9aa4b0381272cb3ebd23ab5"
   },
   {
     "url": "polyfill-b2290f1301185c382ad0.js"

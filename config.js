4366b7c4fbbb2997af5df5413c76abad570fd1f9
Behzad Samadi <behzad.samadi@gmail.com>
2020-07-14 20:47:02 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
-  pathPrefix: '/',
+  url: 'https://elearninghub.github.io',
+  pathPrefix: '/Static-Website-Gatsby-Lumen',
   title: 'Blog by John Doe',
   subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
   copyright: '© All rights reserved.',
